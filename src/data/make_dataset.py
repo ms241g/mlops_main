08617b6f0b4ca51528c4b2a67ab6453ae8c3f5f3
ms241g <manojsharma1122@gmail.com>
2022-05-26 11:26:50 +0530

    Add make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,33 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config= read_params(config_path)
+    print(config)
+    data_path= config["data_source"]["s3_source"]
+    df= pd.read_csv(data_path, sep= ',', encoding='utf-8')
+    print(df.head)
+    return df
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_arg = args.parse_args()
+    data= get_data(config_path=parsed_arg.config)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+ ## read params
+ ## process
+ ## return dataframe
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
-    main()
+
